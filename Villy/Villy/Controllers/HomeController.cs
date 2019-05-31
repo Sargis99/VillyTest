e55ecf53b980bcfdf0a7df38b6d8bdcc80f1af35
Sargis99 <31768511+Sargis99@users.noreply.github.com>
2019-05-31 12:58:12 +0400

    Add files via upload

---
+++ Villy/Villy/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Villy.Models;
+
+namespace Villy.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Service()
+        {
+            ViewData["Message"] = "Your Service page.";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Villy/Villy/Data/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Villy.Data
+{
+    public class Room
+    {
+        public int MyProperty { get; set; }
+        public int dadasdasd { get; set; }
+
+    }
+}
